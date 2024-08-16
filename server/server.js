0fa91ce786a962a37c4566a1a8a52c3694803702
Olesia-Maria Kushka <86930501+Olesia-Mariia@users.noreply.github.com>
2024-08-16 21:44:22 +0300

    deploiment

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -14,16 +15,26 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5001',
-  credentials: true
-}));
+const __dirname = path.resolve();
+
+app.use(
+  cors({
+    origin: "http://localhost:5001",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/auth", authRoutes);
 app.use("/chats", chatsRoutes);
 app.use("/messages", messagesRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   connectDB();
